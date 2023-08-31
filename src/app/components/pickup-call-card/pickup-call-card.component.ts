807bf8332fb267d72635a00c43a7d30068e97ffc
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-08-31 07:02:48 -0600

    added the ngIf function

--- src/app/components/pickup-call-card/pickup-call-card.component.ts
+++ src/app/components/pickup-call-card/pickup-call-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pickup-call-card',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pickup-call-card.component.css']
 })
 export class PickupCallCardComponent implements OnInit {
+
+  @Input() hasHeader:boolean = false;
+  @Input() hasFooter:boolean = false;
+
+  @Input() status:string = "";
+  @Input() upDateAt: string ="";
+  @Input() createdAt: string ="";
+  @Input() notes: string ="";
+  @Input() value: string ="";
+
 
   constructor() { }
 
