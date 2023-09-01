87a72fed51d89346c7ba5ff1789b678bb1ffb296
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-09-01 01:45:52 -0600

    loader page

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
+
+  // beforeEach(waitForAsync()=>{
+  //   TestBed.configurateTestingModule({
+  //     declarations: [LoaderPage],
+  //     imports: [
+  //       IonicModule.forRoot()
+  //       AppRoutingModule
+  //     ]
+  //   })
+  // }
+  // ).compileComponents()
 
   beforeEach(async(() => {
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.get(Router)
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to login page after load', () => {
+    component.ngOnInit
+
+    expect(router.navigate).toHaveBeenCalledWith(['login'])
   });
 });
