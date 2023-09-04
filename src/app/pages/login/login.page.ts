b3e9a04ac5f3165ac8307d84f519453c89ff90d5
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-09-04 05:32:50 -0600

    message forgot username

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,7 @@ import { hide, show } from 'src/store/loading/loading.actions';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/AppState';
 import { recoverPassword } from 'src/store/login/login.action';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -18,11 +19,30 @@ export class LoginPage implements OnInit {
   form: FormGroup;
 
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>,
+    private toastController:ToastController) { }
 
   ngOnInit() {
     this.form = new LoginPageForm(this.formBuilder).createForm();
+    this.store.select("login").subscribe(async loginState =>{
+      if (loginState.isRecoveringPassword){
+        this.store.dispatch(show());
+      }
+      if(loginState.isRecoveredPassword){
+        this.store.dispatch(hide());
+        const toaster = await this.toastController.create({
+          position: "bottom",
+          message: "Correo electrónico de recuperación enviado",
+          color: "primary"
+        });
+        toaster.present();
+      }
+    })
+
+
   }
+
+
 
   forgotEmailPassword(){
     this.store.dispatch(recoverPassword())
